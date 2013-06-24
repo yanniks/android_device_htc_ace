5ae33f7158f4cc21f8d048f75e890702c5665804
yanniks <kontakt@yanniks.de>
2013-06-24 16:08:26 +0200

    added liquid.mk

---
+++ liquid.mk
@@ -0,0 +1,18 @@
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_gsm.mk)
+
+# Release Name
+PRODUCT_RELEASE_NAME := DHD
+
+TARGET_BOOTANIMATION_NAME := 480
+
+# Inherit AOSP device configuration for ace
+$(call inherit-product, device/htc/ace/device_ace.mk)
+
+PRODUCT_NAME := liquid_ace
+PRODUCT_DEVICE := ace
+PRODUCT_BRAND := htc_wwe
+PRODUCT_MODEL := Desire HD
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ace BUILD_FINGERPRINT="google/yakju/maguro:4.2.2/JDQ39/573038:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.2.2 JDQ39 573038 release-keys"
