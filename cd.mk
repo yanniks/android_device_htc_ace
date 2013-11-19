8e3ceb1b18ca4e6d64764a3b9e468a8866a1fd4c
yanniks <kontakt@yanniks.de>
2013-11-19 21:11:17 +0100

    bug fix

---
+++ cd.mk
@@ -0,0 +1,20 @@
+$(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/ace/device.mk)
+
+PRODUCT_DEVICE := ace
+PRODUCT_NAME := cd_ace
+PRODUCT_BRAND := htc_wwe
+PRODUCT_MODEL := Desire HD
+PRODUCT_MANUFACTURER := HTC
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ace BUILD_FINGERPRINT="google/yakju/maguro:4.2.2/JDQ39/573038:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.2.2 JDQ39 573038 release-keys"
+
+# Release Name
+PRODUCT_RELEASE_NAME := ace
+
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
